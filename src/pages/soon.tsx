4e0291a1b4e5dc9fd5426d7580f704a62abcaf88
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 16:01:41 -0400

    add full logo and place in coming soon page + img styles

--- src/pages/soon.tsx
+++ src/pages/soon.tsx
@@ -1,7 +1,9 @@
+import FullLogo from '../assets/SSMC_full_logo.svg'
 function ComingSoon(){
     return(
         <>
             <h1>Great works are coming...</h1>
+            <p><img src={FullLogo} className='imgH_25'/></p>
             <p><i>He who began a good work in you will carry it on to completion until the day of Christ Jesus.</i> <strong>--Philippians 1:6</strong></p>
             <br />
             <p>There is a good work to be done through the ministry of Sweet Surrender Ministry Center and a newly constructed website is just part of the beginning! </p>
