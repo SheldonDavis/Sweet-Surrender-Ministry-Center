e3f021a45787564b9db9b35076951d2fe7b8213f
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 15:30:21 -0400

    create contact information

---
+++ src/components/MailingInfo.tsx
@@ -0,0 +1,9 @@
+function Mail(){
+    return(
+        <article>
+            <h3>SSMC mailing address</h3>
+            <p>P.O. Box #123321 <br/> Alum Bank, PA 15521</p>
+        </article>
+    )
+}
+export default Mail
---
+++ src/components/PhoneInfo.tsx
@@ -0,0 +1,9 @@
+function Phone(){
+    return(
+        <article>
+            <h3>Text or Call SSMC</h3>
+            <p>(582) 855-9583</p>
+        </article>
+    )
+}
+export default Phone
