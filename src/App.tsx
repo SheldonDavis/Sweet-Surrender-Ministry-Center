ccd96ee3a25fbac624016eedafc12388d14999fd
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 08:36:34 -0400

    add conditional for coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import SiteRoutes from "./components/routes";
 import Nav from "./components/Header";
-
+import ComingSoon from "./pages/soon";
+const isLive = false
 function App(){
   return(
-    <>
-      <Nav/>
-      <SiteRoutes/>
-    </>
+    isLive?(
+      <>
+        <Nav/>
+        <SiteRoutes/>
+      </>
+    ):(
+      <ComingSoon/>
+    )
   )
+    
+  
 }
 export default App;
