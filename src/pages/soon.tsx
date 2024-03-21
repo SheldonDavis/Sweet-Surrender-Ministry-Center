533c487ba6421bb69fcde8082a68fc322e1d5f87
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 15:31:14 -0400

    content for coming soon page

--- src/pages/soon.tsx
+++ src/pages/soon.tsx
@@ -2,6 +2,11 @@ function ComingSoon(){
     return(
         <>
             <h1>Great works are coming...</h1>
+            <p><i>He who began a good work in you will carry it on to completion until the day of Christ Jesus.</i> <strong>--Philippians 1:6</strong></p>
+            <br />
+            <p>There is a good work to be done through the ministry of Sweet Surrender Ministry Center and a newly constructed website is just part of the beginning! </p>
+            <br/>
+            <p>Please check back soon! </p>
         </>
     )
 }
