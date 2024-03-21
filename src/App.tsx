77d34160337b7193a1e478494e85cc78888403f0
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 13:11:08 -0400

    show coming soon if variable is set to true or false

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import SiteRoutes from "./components/routes";
 import Nav from "./components/Header";
 import ComingSoon from "./pages/soon";
-const isLive = false
+const isLive = import.meta.env.VITE_IS_LIVE === "true"
 function App(){
   return(
     isLive?(
