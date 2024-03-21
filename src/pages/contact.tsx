1958f8709f565edc1bd6b8a04aca95e5937db3f2
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-21 15:31:55 -0400

    contact page content addition

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,21 @@
 //components
 import QuestionsForm from "../components/QuestionsForm";
+import Phone from "../components/PhoneInfo";
+import Mail from "../components/mailingInfo";
+
 
 function Contact(){
 
 
   return(
       <>
-          <h1>~Contact~</h1>
-          <QuestionsForm/>
+        <article>
+            <h1>Contact</h1>
+            <p>descriptive text for the contact page. tell people about ways to contact SSMC, etc.</p>
+        </article>
+        <Phone/>
+        <Mail/>
+        <QuestionsForm/>
       </>
   )
 }
