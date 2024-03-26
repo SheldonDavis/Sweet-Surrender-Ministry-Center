8a1624446ef39d408051a36dff19a8e31e3ba7bb
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-26 15:06:19 -0400

    link to legal notice guidelines and additional legal link options

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <footer>
-            <p><a href={`/legal`}>legal</a> | <a href={`#`}>terms & conditions</a></p>
+            <p><a href={`/legal`}>legal</a> | <a href={`/legal`}>terms & conditions</a> | <a href={`/legal`}>privacy notice</a></p>
             <p className={`notes`}><i>What legal type pages are needed??</i></p>
         </footer>
     )
--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -4,6 +4,7 @@ function Legal(){
         <>
             <h1>~Legal~</h1>
             <p>legal disclaimers and other stuff needed.</p>
+            <p><a href={`https://charitylawyerblog.com/2019/12/23/quick-guide-to-writing-nonprofit-privacy-and-legal-notice-webpage/`} target="blank_">take a look at this legal documentation guideline?</a></p>
         </>
     )
 }
