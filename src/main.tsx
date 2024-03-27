7deb9cf83b87c5d659ff10fe66e70fbc36f3373e
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-27 12:23:10 -0400

    change references to stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App.tsx'
+import App from './App.tsx';
 // import './style/index.css'
 // import './style/App.css'
-import './style/style.css'
+// import './style/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
