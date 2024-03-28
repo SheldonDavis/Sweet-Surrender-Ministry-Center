2eebb14a97f3df9e034ff4457d52eda29b56e5df
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-28 13:42:40 -0400

    add main tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import SiteRoutes from "./components/routes";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ComingSoon from "./pages/soon";
+import SiteRoutes from './components/routes';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ComingSoon from './pages/soon';
 
-const isLive = import.meta.env.VITE_IS_LIVE === "true"
-function App(){
-  return(
-    isLive?(
-      <>
-        <Header/>
-        <SiteRoutes/>
-        <Footer/>
-      </>
-    ):(
-      <ComingSoon/>
-    )
-  )
-    
-  
+const isLive = import.meta.env.VITE_IS_LIVE === 'true';
+function App() {
+  return isLive ? (
+    <>
+      <Header />
+      <main>
+        <SiteRoutes />
+      </main>
+      <Footer />
+    </>
+  ) : (
+    <ComingSoon />
+  );
 }
 export default App;
