279e847756a6bcd7e04cca9bc83cf92ff68363e9
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-03-28 20:35:05 -0400

    create reuseable component for popup modal

---
+++ src/components/DialogPopup.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+interface Properties {
+  openBtnTxt: string;
+  ModalTitle?: string;
+  BodyText: Array<string>;
+}
+
+function DialogPopup(props: Properties) {
+  const { openBtnTxt, ModalTitle, BodyText } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  function handle_toggleOpen() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <>
+      <dialog open={isOpen}>
+        {ModalTitle && <h3>{ModalTitle}</h3>}
+        {BodyText.map((p, i) => {
+          return <p key={i}>{p}</p>;
+        })}
+        <p>
+          <a
+            className={`btn`}
+            onClick={() => {
+              handle_toggleOpen();
+            }}
+          >
+            Close
+          </a>
+        </p>
+      </dialog>
+      <p>
+        <a
+          className={`btn`}
+          onClick={() => {
+            handle_toggleOpen();
+          }}
+        >
+          {openBtnTxt}
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default DialogPopup;
