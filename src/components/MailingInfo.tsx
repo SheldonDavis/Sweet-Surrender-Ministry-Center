b3538f8f2ac771ebdfe46fe40456efd63c67fb32
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-04-08 08:22:39 -0400

    text update

--- src/components/MailingInfo.tsx
+++ src/components/MailingInfo.tsx
@@ -3,7 +3,7 @@ function Mail() {
     <section className={`coloredBG light`}>
       <h3>Mailing Address</h3>
       <p>
-        P.O. Box #206 <br /> Alum Bank, PA 15521
+        P.O. Box 206 <br /> Alum Bank, PA 15521
       </p>
     </section>
   );
