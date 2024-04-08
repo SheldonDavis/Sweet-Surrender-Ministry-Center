9043ca1fb13a24b31cc0bd4f1e589ac77280cf90
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-04-08 08:22:32 -0400

    update formatting

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 //importing pages
 import Whoops from '../pages/404';
@@ -9,37 +9,28 @@ import Donate from '../pages/donate';
 import Legal from '../pages/legal';
 import Projects from '../pages/projects';
 
-
 const SiteRoutes = () => {
-    return (
-        <Routes>
-            //* 404 page to handle all weird URLS *//
-            <Route path='*' element={<Whoops/>}/>
-            
-            //* home page pathing *//
-            <Route path={'/'} element={<Home/>}/>
-
-            //* about page pathing *//
-            {/* <Route path={'/about'} element={<About/>}/> */}
-
-            //* about page detail pathing *//
-            {/* <Route path='/about/:id' Component={(props)=>(
+  return (
+    <Routes>
+      //* 404 page to handle all weird URLS *//
+      <Route path='*' element={<Whoops />} />
+      //* home page pathing *//
+      <Route path={'/'} element={<Home />} />
+      //* about page pathing *//
+      {/* <Route path={'/about'} element={<About/>}/> */}
+      //* about page detail pathing *//
+      {/* <Route path='/about/:id' Component={(props)=>(
                 <About {...props} />
             )}/> */}
-
-            //* Contact page route *//
-            <Route path={`/contact`} element={<Contact/>}/>
-
-            //* Donation  information page route *//
-            <Route path={`/donate`} element={<Donate/>}/>
-
-            //* Legal information page route *//
-            <Route path={`/legal`} element={<Legal/>}/>
-
-            //* Route for listing projects page route *//
-            <Route path={`/projects`} element={<Projects/>}/>
-            
-        </Routes>
-    )
-}
+      //* Contact page route *//
+      <Route path={`/contact`} element={<Contact />} />
+      //* Donation information page route *//
+      <Route path={`/donate`} element={<Donate />} />
+      //* Legal information page route *//
+      <Route path={`/legal`} element={<Legal />} />
+      //* Route for listing projects page route *//
+      <Route path={`/projects`} element={<Projects />} />
+    </Routes>
+  );
+};
 export default SiteRoutes;
