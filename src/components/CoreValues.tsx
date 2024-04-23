18214f0fcb605d7ee0da837c442960f8078adf03
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-04-22 22:40:17 -0400

    formatting change

--- src/components/CoreValues.tsx
+++ src/components/CoreValues.tsx
@@ -9,34 +9,33 @@ function CoreValues(props: Properties) {
       <p>
         <strong>
           <u>See them.</u>
-        </strong>{' '}
-        <i>Hebrews 2:10 (NLT)</i> - God, for whom and through whom everything
-        was made...
+        </strong>
+        <br />
+        <i>Hebrews 2:10 </i> - God, for whom and through whom everything was made...
       </p>
       <p>
         <strong>
           <u>Serve them.</u>
-        </strong>{' '}
-        <i>Matthew 22:37-39 (NLT)</i> - Love the LORD your God with all your
-        heart, all your soul, and all your mind... Love your neighbor as
-        yourself.
+        </strong>
+        <br />
+        <i>Matthew 22:37-39 </i> - Love the LORD your God with all your heart, all your soul, and all your mind... Love your neighbor as yourself.
       </p>
 
       <p>
         <strong>
           <u>Affirm them.</u>
-        </strong>{' '}
+        </strong>
+        <br />
         <i>Acts 17:28</i> - For in Him, we live and move and have our being.
       </p>
 
       <p>
         <strong>
           <u>Seek wisdom.</u>
-        </strong>{' '}
-        <i>James 3:17 (NLT)</i> - the wisdom from above is first of all pure. It
-        is also peace loving, gentle at all times, and willing to yield to
-        others. It is full of mercy and the fruit of good deeds. It shows no
-        favoritism and is always sincere.
+        </strong>
+        <br />
+        <i>James 3:17 </i> - the wisdom from above is first of all pure. It is also peace loving, gentle at all times, and willing to yield to others. It is full of mercy and the fruit of good deeds. <br />
+        It shows no favoritism and is always sincere.
       </p>
     </section>
   );
