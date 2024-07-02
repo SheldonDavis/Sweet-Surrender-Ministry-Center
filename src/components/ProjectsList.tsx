c45210a2eb9a00a872408a27b9b88f779dffe668
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-07-02 09:44:57 -0400

    read more handling

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -6,12 +6,16 @@ import MannaMonday from '../assets/MannaMonday.jpg';
 import SecondChance from '../assets/SecondChance.jpg';
 
 function ProjectsList() {
-  const projectData = [
+  const currentProjects = [
     {
+      id: `1`,
       title: `Birthday Blessing Box`,
       image: birthdayBlessingBox,
       description: [
-        `A birthday party in a cakebox is given to elementary school aged children who may not otherwise receive a celebration for their special day. Boxes include a cake mix, round pan, can of soda for cake mixture with recipe card, icing, candles, birthday card, crown, stickers, a bow, party favors, fun socks, age-appropriate STEM activity, and other small gifts. Boxes are distributed through partnering local school districts.`,
+        `A birthday party in a cakebox is given to elementary school aged children who may not otherwise receive a celebration for their special day.  `,
+      ],
+      readMore: [
+        `Boxes include a cake mix, round pan, can of soda for cake mixture with recipe card, icing, candles, birthday card, crown, stickers, a bow, party favors, fun socks, age-appropriate STEM activity, and other small gifts.`,
       ],
       wishlist: [
         {
@@ -21,11 +25,13 @@ function ProjectsList() {
       ],
     },
     {
+      id: `2`,
       title: `H.O.P.E. Box`,
       image: HopeBox,
       description: [
         `Boxes are available to individuals and families whether out of an emergency crisis or needs-based for: food staples, basic hygiene supplies, household cleaning products, necessary travel gas cards, or other circumstantial necessities.`,
       ],
+      readMore: [],
       wishlist: [
         {
           btnTxt: `HOPE Box Gift Cards wishlist`,
@@ -45,12 +51,16 @@ function ProjectsList() {
         },
       ],
     },
+  ];
+  const futureProjects = [
     {
+      id: `3`,
       title: `Manna Monday`,
       image: MannaMonday,
       description: [
-        `Beginning with one Monday per month, free lunch of sustenance is offered to anyone whether by need of physical hunger, social hunger, or spiritual hunger. Packaged meal distribution takes place at a public community site or civic organization location until a permanent location is secured with a dine-in community space.`,
+        `Beginning with one Monday per month, free meal of sustenance is offered to anyone whether by need of physical hunger, social hunger, or spiritual hunger.`,
       ],
+      readMore: [],
       wishlist: [
         {
           btnTxt: `Manna Monday wishlist`,
@@ -59,10 +69,14 @@ function ProjectsList() {
       ],
     },
     {
+      id: `4`,
       title: `Second Chance`,
       image: SecondChance,
       description: [
-        `A mentorship style program for individuals who have struggled on their own to overcome barriers and who present a true desire to change life patterns toward becoming productive members of their community. Mentors meet with the individual first to establish a goals plan followed by a yearlong schedule of progressive accountability meetings with Biblical application. Upon successful completion, individuals will have learned certain life skills and/or trades to have reached or be well established on their way to attain their original goals plan. `,
+        `A mentorship style program for individuals who have struggled on their own to overcome barriers and who present a true desire to change life patterns toward becoming productive members of their community.`,
+      ],
+      readMore: [
+        `Mentors meet with the individual first to establish a goals plan followed by a yearlong schedule of progressive accountability meetings with Biblical application. Upon successful completion, individuals will have learned certain life skills and/or trades to have reached or be well established on their way to attain their original goals plan.`,
       ],
       wishlist: [
         {
@@ -73,22 +87,61 @@ function ProjectsList() {
     },
   ];
   return (
-    <section id={`ProjectList`}>
-      <h1>Projects</h1>
-      {projectData.map((project, i) => {
-        return (
-          <Project
-            key={i}
-            title={project.title}
-            image={project.image}
-            description={project.description}
-            wishlist={project?.wishlist}
-            orientation={(i + 1) % 2 != 0}
-          />
-        );
-      })}
-      {/* <Project title={`testing`} image={`file path`} description={[`this`,`is`,`it`]} orientation={true} /> */}
-    </section>
+    <>
+      <section id={`ProjectList`}>
+        <h1>Projects</h1>
+
+        <div className={`separator`}>
+          <span></span>
+        </div>
+
+        <h2>Current Projects</h2>
+        {currentProjects.map((project, i) => {
+          return (
+            <Project
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              image={project.image}
+              description={project.description}
+              readMore={project.readMore}
+              wishlist={project?.wishlist}
+              orientation={(i + 1) % 2 != 0}
+            />
+          );
+        })}
+
+        <div className={`separator`}>
+          <span></span>
+        </div>
+
+        <h2>Future Projects</h2>
+        {futureProjects.map((project, i) => {
+          return (
+            <Project
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              image={project.image}
+              description={project.description}
+              readMore={project.readMore}
+              wishlist={project?.wishlist}
+              orientation={(i + 1) % 2 != 0}
+            />
+          );
+        })}
+
+        {/* <Project title={`testing`} image={`file path`} description={[`this`,`is`,`it`]} orientation={true} /> */}
+      </section>
+      <section id={`ExpandingOpportunities`} className={`coloredBG light`}>
+        <h2>Expanding Opportunities</h2>
+        <p>
+          Some of the envisioned growth for the future location of SSMC are: Loads of Love laundry assistance, Community Garden space, Transitional
+          Housing in connection with the Second Chance program, and meeting space available to outside human service programs such as AA, Celebrate
+          Recovery, Stephen Ministries, Grief Share, or other programs of the like.
+        </p>
+      </section>
+    </>
   );
 }
 export default ProjectsList;
