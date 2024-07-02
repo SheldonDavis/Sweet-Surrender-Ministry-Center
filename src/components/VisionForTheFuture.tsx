424e0c65ce38f1ee27cd49f57db90a16e3426ab7
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-07-02 09:45:03 -0400

    text changes

--- src/components/VisionForTheFuture.tsx
+++ src/components/VisionForTheFuture.tsx
@@ -8,15 +8,8 @@ function VisionForTheFuture(props: Properties) {
     <section id={`VisionOfTheFuture`} className={classes}>
       <h3>Vision for the Future of SSMC</h3>
       <p>
-        With anticipated growth stemming from our four start-up projects, the
-        overarching vision is to secure a physical brick and mortar location for
-        expanded operations of existing projects and evolving opportunities
-        offered for those we serve. Some of the envisioned growth for the future
-        location of SSMC are: Loads of Love laundry assistance, Community Garden
-        space, Transitional Housing in connection with the Second Chance
-        program, and meeting space available to outside human service programs
-        such as AA, Celebrate Recovery, Stephen Ministries, Grief Share, or
-        other programs of the like.
+        The overarching vision is to secure a physical brick and mortar location for expanded operations of current and future projects and evolving
+        opportunities offered for those we serve.
       </p>
     </section>
   );
