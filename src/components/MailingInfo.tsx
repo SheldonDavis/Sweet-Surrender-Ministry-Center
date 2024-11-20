91b86f236dbcbad94fc9ac35b309fbeaf167071e
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-11-20 13:28:00 -0500

    css tweaks and updates to content

--- src/components/MailingInfo.tsx
+++ src/components/MailingInfo.tsx
@@ -1,10 +1,11 @@
 function Mail() {
   return (
     <section className={`coloredBG light`}>
-      <h3>Mailing Address</h3>
+      <h3>Location Address</h3>
       <p>
         108 Follmar Lane <br /> Alum Bank, PA 15521
       </p>
+      <h3>Mailing Address</h3>
       <p>
         P.O. Box 206 <br /> Alum Bank, PA 15521
       </p>
