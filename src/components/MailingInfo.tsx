2884b787ce93f62520aaee2d505df0407091cdc9
Sheldon Davis <sheldon.davis1515@gmail.com>
2025-02-05 08:51:40 -0500

    added open time and reference text

--- src/components/MailingInfo.tsx
+++ src/components/MailingInfo.tsx
@@ -5,6 +5,13 @@ function Mail() {
       <p>
         108 Follmar Lane <br /> Alum Bank, PA 15521
       </p>
+      <p>
+        Open Mondays
+        <br />
+        10 a.m. to 3 p.m.
+        <br />
+        Or Call to Schedule a Time
+      </p>
       <h3>Mailing Address</h3>
       <p>
         P.O. Box 206 <br /> Alum Bank, PA 15521
